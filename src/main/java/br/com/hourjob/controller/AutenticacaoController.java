b389d6ea7dfc8d7af2a23f760dff1b934aa8e359
Roberto Vitor <robertovitor@gmail.com>
2020-05-30 14:06:25 -0300

    Cloud

--- src/main/java/br/com/hourjob/controller/AutenticacaoController.java
+++ src/main/java/br/com/hourjob/controller/AutenticacaoController.java
@@ -1,7 +1,8 @@
 package br.com.hourjob.controller;
 
-import javax.validation.Valid;
-
+import br.com.hourjob.controller.form.LoginForm;
+import br.com.hourjob.dto.TokenDto;
+import br.com.hourjob.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,17 +10,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import br.com.hourjob.controller.form.LoginForm;
-import br.com.hourjob.dto.TokenDto;
-import br.com.hourjob.service.TokenService;
+import javax.validation.Valid;
 
 @RestController
-@RequestMapping("auth")
+@RequestMapping("/auth")
 public class AutenticacaoController {
 
 	@Autowired
@@ -43,5 +39,10 @@ public class AutenticacaoController {
 		} catch (AuthenticationException e) {
 			return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
 		}
+
 	}
+  @GetMapping("/encrypt/{pass}")
+  public String getEncrypt(@PathVariable(name = "pass") String pass) {
+    return tokenService.getEncrypt(pass);
+  }
 }
