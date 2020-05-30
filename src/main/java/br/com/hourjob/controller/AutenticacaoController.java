f352525d7cc46a7078a486c43b1a1a6e06301f4f
Roberto Vitor <robertovitor@gmail.com>
2020-05-30 14:12:50 -0300

    Cloud

--- src/main/java/br/com/hourjob/controller/AutenticacaoController.java
+++ src/main/java/br/com/hourjob/controller/AutenticacaoController.java
@@ -1,7 +1,8 @@
 package br.com.hourjob.controller;
 
-import javax.validation.Valid;
-
+import br.com.hourjob.controller.form.LoginForm;
+import br.com.hourjob.dto.TokenDto;
+import br.com.hourjob.service.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,14 +10,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import br.com.hourjob.controller.form.LoginForm;
-import br.com.hourjob.dto.TokenDto;
-import br.com.hourjob.service.TokenService;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
